d10809d4ef20ea8c4defdc572e6362bd7b86431c
Sviatoslav Khrystyna <sviatoslavxp@gmail.com>
2020-01-01 16:01:01 +0200

    feat(authentication): add user model and repository

---
+++ server/src/main/java/com/khrystyna/nerdysoft/models/User.java
@@ -0,0 +1,16 @@
+package com.khrystyna.nerdysoft.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    private Long id;
+    private String username;
+    private String password;
+}
---
+++ server/src/main/java/com/khrystyna/nerdysoft/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.khrystyna.nerdysoft.repository;
+
+import com.khrystyna.nerdysoft.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
