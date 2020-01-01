120a98e6e205d30951823f170e96e98ea64b856f
Sviatoslav Khrystyna <sviatoslavxp@gmail.com>
2020-01-01 19:38:23 +0200

    fix(authentication): remove log

--- server/src/main/java/com/khrystyna/nerdysoft/controller/AuthenticationController.java
+++ server/src/main/java/com/khrystyna/nerdysoft/controller/AuthenticationController.java
@@ -4,7 +4,6 @@ import com.khrystyna.nerdysoft.dto.SignUpDto;
 import com.khrystyna.nerdysoft.models.User;
 import com.khrystyna.nerdysoft.service.interfaces.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,12 +22,11 @@ public class AuthenticationController {
 
     @PostMapping("/sign-up")
     public ResponseEntity<Object> signUp(@RequestBody SignUpDto signUpDto) {
-        log.error(signUpDto.toString());
         userService.save(User.builder()
                 .username(signUpDto.getUsername())
                 .password(signUpDto.getPassword())
                 .build());
-        return ResponseEntity.status(HttpStatus.OK).body("im good");
+        return ResponseEntity.ok().build();
     }
 
 }
